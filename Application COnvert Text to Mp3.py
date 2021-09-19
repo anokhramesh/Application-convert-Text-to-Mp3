dc43865b0ccb847f2952d4c7bf70f3d5711e3a2e
anokhramesh <67797819+anokhramesh@users.noreply.github.com>
2021-09-19 21:55:35 +0400

    Update Application COnvert Text to Mp3.py

--- Application COnvert Text to Mp3.py
+++ Application COnvert Text to Mp3.py
@@ -30,8 +30,8 @@ text=ScrolledText(root,width=30,height=10,wrap=WORD,padx=10,pady=10,bd=5,relief=
 text.grid(row=0,columnspan=3)
 #button for listen the text you entered on the scrolltext area
 ttk.Button(root,text='Listen',width=7,command=speak).grid(row=1,column=0,ipadx=2)
-
+# button for Clear the Text
 ttk.Button(root,text='Clear',width=7,command=lambda :text.delete('0.0',END)).grid(row=1,column=1,ipadx=2)
-
+#button for Save the text to Mp3
 ttk.Button(root,text='Save',width=7,command=save).grid(row=1,column=2,ipadx=2)
 root.mainloop()
